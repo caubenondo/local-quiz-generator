206d09ceba8777d31b55e4b80272bf8b1f1d0a23
Hai iMac15 <caubenondo@gmail.com>
2022-04-10 15:54:44 -0700

    hook up the timer,user score  and prompt question only timer>0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
-console.log("script is hooked up");
-
+/* VARIABLES DELACRE AREA */
 const quizContainerEl = document.querySelector(".quiz-container");
 const quizAreaEl = document.querySelector(".quiz-area");
 // placeholder for question we are currently quizin
 let currentQuestion = {};
 let askingQueue = [];
-
-
+const quizTime = 10;
+let timer = 0;
+const timerDisplayEl = document.querySelector(".progressBar");
 const quizBank = [];
 const choices = ["a", "b", "c", "d"];
+let userScore = 0;
+
+const userScoreEl = document.querySelector('.userScore');
+const stopWatchButton = document.querySelector('#stopWatch');
+stopWatchButton.addEventListener('click', timeWatcher);
+
+// sample questions
 const q1 = {
   prompt: "What is my fav food?",
   answers: {
@@ -17,7 +24,7 @@ const q1 = {
     c: "Pho",
     d: "Sanwiches",
   },
-  correct: "c"
+  correct: "c",
 };
 
 const q2 = {
@@ -28,22 +35,22 @@ const q2 = {
     c: "C",
     d: "D",
   },
-  correct: "c"
+  correct: "c",
 };
 
+// this function will load/retrieve quizbank from local storage and shuffle the question order
 function dataHandler() {
   // note: JSON doesnt save methods of object
 
-  quizBank.push(q1);
-/*   quizBank.push(q2);
-
-//   localstorage setter -- quizBank
-  localStorage.setItem("quizBank", JSON.stringify(quizBank)); */
+  // quizBank.push(q1);
+  /*  quizBank.push(q2);
+      localstorage setter -- quizBank
+      localStorage.setItem("quizBank", JSON.stringify(quizBank)); */
 
   // localstorage geter -- quizBank
   const localQuizBank = JSON.parse(localStorage.getItem("quizBank")) || [];
   console.log("LOCAL QUIZ BANK");
-  console.table(localQuizBank);
+  // console.table(localQuizBank);
   askingQueue = localQuizBank;
   shuffleArray(askingQueue);
   console.table(askingQueue);
@@ -89,22 +96,27 @@ function submitAnswerHandler(event) {
     //grab user's choice
     let choice = element.dataset.selected;
     console.log(choice);
-    
+
     if (isCorrect(choice)) {
       console.log("Correct answer - handle logic when user pick right answer.");
       // display feedback right
       // reward user 2s
       // update score
       // queue in the next question
+      timer +=5;
+      userScore +=10;
+      displayUserScore();
       askingQueue.shift();
       askQuestion();
-
     } else {
       console.log("wrong answer - handle logic when user picks wrong answers");
       // display "wrong"
       // minus 5s on the time counter
       // updatescore
       // queue in the next question
+      timer -=2;
+      userScore -=5;
+      displayUserScore();
       askingQueue.shift();
       askQuestion();
     }
@@ -128,58 +140,116 @@ function shuffleArray(array) {
   }
 }
 
-function isCorrect(input){
-    return input === currentQuestion.correct? true :false;
+function isCorrect(input) {
+  return input === currentQuestion.correct ? true : false;
 }
 
-function askQuestion(){
-    if(askingQueue.length!==0){
-        currentQuestion = askingQueue[0];
-        displayCurrentQuestion(currentQuestion);
-
-    }else{
-        quizAreaEl.textContent = 'No Question';
-    }
+function askQuestion() {
+  if (askingQueue.length !== 0 && timer>0) {
+    currentQuestion = askingQueue[0];
+    displayCurrentQuestion(currentQuestion);
+  } else {
+    quizAreaEl.textContent = "No Question";
+  }
 }
 
+function finalScoreDisplay(){
+  quizAreaEl.innerHTML = `
+    <p>Display userScore and ask if they want to add to records</p>
+  `;
+}
 
-function init(){
-    dataHandler();
-    askQuestion();
+function init() {
+  updateTimerDisplay(0,0,false);
+  dataHandler();
+  askQuestion();
+  
 }
 
 init();
 
+function displayUserScore(){
+  userScoreEl.innerHTML = `<h5>Score: ${userScore}</h5>`;
+}
+
+/* TIMER / COUNTER - Time handling*/
 
 
-document.querySelector('#addQuestion').addEventListener('click', addQuestionToBank);
+function timeWatcher() {
+  stopWatchButton.disabled = true;
+  timer = quizTime;
+  userScore = 0;
+  askQuestion();
+  displayUserScore();
+  const everyMinute = setInterval(() => {
+    timer--;
+    updateTimerDisplay(quizTime, timer,true);
 
-function addQuestionToBank(){
-    const q = document.querySelector('#addPrompt').value;
-    const a = document.querySelector('#addAnswerA').value;
-    const b = document.querySelector('#addAnswerB').value;
-    const c = document.querySelector('#addAnswerC').value;
-    const d = document.querySelector('#addAnswerD').value;
-    const correct = document.querySelector('#correctAnswer');
-    console.log(correct);
+    if (timer <= 0) {
+      updateTimerDisplay(quizTime, timer,false);
+      finalScoreDisplay()
+      clearInterval(everyMinute);
+    }
+  }, 1000);
+}
+function updateTimerDisplay(maxTime, timeLeft, isRunning) {
+  let htmlTemplate =``
+  if (isRunning == true) {
+    htmlTemplate = `
+      <label for='timer'>${timeLeft} s</label>
+      <progress id='timer' value='${timeLeft}' max='${maxTime}'></progress>
+    `;
+   
+  }
+  else{
+    htmlTemplate = `<p> NO TIME LEFT</p`;
+    stopWatchButton.disabled = false;
+  }
+  timerDisplayEl.innerHTML = htmlTemplate;
+}
 
-    const newQuestion = {prompt: q,
+/* ADDING QUESTION FORM */
+document
+  .querySelector("#addQuestion")
+  .addEventListener("click", addQuestionToBank);
+
+function addQuestionToBank() {
+  // target all the form's inputs and get their value
+  const q = document.querySelector("#addPrompt").value;
+  const a = document.querySelector("#addAnswerA").value;
+  const b = document.querySelector("#addAnswerB").value;
+  const c = document.querySelector("#addAnswerC").value;
+  const d = document.querySelector("#addAnswerD").value;
+  // except this one because we need to clear it later with seletedIndex,
+  // it will be more efficient if we only get element ( not its direct value)
+  const correct = document.querySelector("#correctAnswer");
+
+  // create a new question object based on whatever user put into the form
+  const newQuestion = {
+    prompt: q,
     answers: {
-      'a': a,
-      'b':b,
-      'c':c,
-      'd':d,
+      a: a,
+      b: b,
+      c: c,
+      d: d,
     },
-    correct: correct.value
+    correct: correct.value,
   };
-  let localBank = JSON.parse(localStorage.getItem('quizBank')) || [];
+  /* // add question to the localstorage by 
+          retreiving/parsing localstoreage string, 
+          adding question to array, 
+          stringtify and store it back to localstorage
+  */
+  let localBank = JSON.parse(localStorage.getItem("quizBank")) || [];
   localBank.push(newQuestion);
-  localStorage.setItem('quizBank', JSON.stringify(localBank));
-  const inputs = document.querySelectorAll('.addQuestionForm input');
-  console.log(inputs);
-  inputs.forEach(input => {
-      input.value = '';
-  });
-  correct.selectedIndex=0;
+  localStorage.setItem("quizBank", JSON.stringify(localBank));
 
+  // clear or reset input values
+  const inputs = document.querySelectorAll(".addQuestionForm input");
+  console.log(inputs);
+  inputs.forEach((input) => {
+    input.value = "";
+  });
+  correct.selectedIndex = 0;
+  // end clearing
 }
